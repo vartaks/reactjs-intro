e4ba2467fd0fef86b599c262b3695fe151d7b2e1
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-10-14 13:49:15 +0530

    Used the function syntax for setState to avoid potential deadlock while updating state based on previous state value

--- React_Game/js/app.js
+++ React_Game/js/app.js
@@ -13,9 +13,9 @@ class Button extends React.Component {
     }
 
     handleClick() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1
+        }));
     };
 
     render() {
