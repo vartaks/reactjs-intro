22ad814a70b174d35603d640d7bf30e6567f161f
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-10-21 11:53:38 +0530

    Added the Result function component and re-used Button class component to increment by different values

--- React_Game/js/app.js
+++ React_Game/js/app.js
@@ -2,29 +2,52 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 class Button extends React.Component {
+
+    render() {
+        return (
+            <button 
+                onClick={() => this.props.onClickFunction(this.props.incrementValue)}>
+                +{this.props.incrementValue}
+            </button>
+        );
+    }
+}
+
+const Result = (props) => {
+    return (
+        <div>Value is {props.counter}</div>
+    );
+};
+
+class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
+        this.incrementCounter = this.incrementCounter.bind(this);
 
         this.state = { 
             counter: 0 
         };
     }
 
-    handleClick() {
+    incrementCounter(incrementValue) {
         this.setState((prevState) => ({
-            counter: prevState.counter + 1
+            counter: prevState.counter + incrementValue
         }));
     };
 
     render() {
         return (
-            <button onClick={this.handleClick}>
-            {this.props.label} {this.state.counter}
-            </button>
+            <div>
+                <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+                <Button incrementValue={5} onClickFunction={this.incrementCounter} />
+                <Button incrementValue={10} onClickFunction={this.incrementCounter} />
+                <Button incrementValue={100} onClickFunction={this.incrementCounter} />
+                
+                <Result counter={this.state.counter}/>
+            </div>
         );
     }
 }
 
-ReactDOM.render(<Button label='Value = '/>,  document.getElementById('app'));
+ReactDOM.render(<App />,  document.getElementById('app'));
