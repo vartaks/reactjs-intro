bdecf199cec992085eb9e21bf7657eafdb7ff9cb
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-11-10 10:37:41 +0530

    Added the Card and CardList components

--- Github_Cards/js/app.js
+++ Github_Cards/js/app.js
@@ -3,8 +3,29 @@ var ReactDOM = require('react-dom');
 
 const Card = (props) => {
     return (
-        <h1>Value is awesome!</h1>
+        <div style={{margin:'1em'}}>
+            <img width='75' src={props.avatar_url} />
+            <div style={{display:'inline-block', marginLeft:10}}>
+                <div style={{fontSize:'1.25em', fontWeight:'bold'}}>
+                    {props.name}
+                </div>
+                <div>
+                    {props.company}
+                </div>
+            </div>
+        </div>
     );
 };
 
-ReactDOM.render(<Card />,  document.getElementById('app'));
+const CardList = (props) => {
+    return (
+        <div>
+            <Card name='Paul O’Shannessy'
+                avatar_url='https://avatars1.githubusercontent.com/u/8445?v=4'
+                company='Facebook'
+            />
+        </div>
+    );
+}
+
+ReactDOM.render(<CardList />,  document.getElementById('app'));
