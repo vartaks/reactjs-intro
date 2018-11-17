abe81fb8a92c1d450237cf64cc64030470ede7e2
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-11-17 16:44:38 +0530

    Added bootstrap CSS and JS logic for generating random number of stars

--- Play_Nine/js/app.js
+++ Play_Nine/js/app.js
@@ -2,27 +2,49 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 const Stars = (props) => {
+
+    // Random number of stars between 1 and 9
+    const numberOfStars = 1 + Math.floor(Math.random() * 9);
+
+    let stars = [];
+
+    for (let i=0; i<numberOfStars; i++) {
+        stars.push(<i key={i} className="fa fa-star"></i>);
+    }
+
     return (
-        <div>
-            stars...
+        <div className="col-5">
+            {stars}
         </div>
     );
 }
 
 const Button = (props) => {
     return (
-        <div>
-            butt...
+        <div className="col-2">
+            <button>=</button>
         </div>
     );
 }
 
 const Answer = (props) => {
     return (
-        <div>
+        <div className="col-5">
             answer...
         </div>
     )
+}
+
+const Numbers = (props) => {
+    return (
+        <div className="card text-center">
+            <div>
+                <span>1</span>
+                <span className="selected">2</span>
+                <span className="used">3</span>
+            </div>
+        </div>
+    );
 }
 
 class Game extends React.Component {
@@ -32,11 +54,16 @@ class Game extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="container">
                 <h3>Play Nine!</h3>
-                <Stars />
-                <Button />
-                <Answer />
+                <hr />
+                <div className="row">
+                    <Stars />
+                    <Button />
+                    <Answer />
+                </div>
+                <br />
+                <Numbers />
             </div>
         );
     }
